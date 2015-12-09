590a50f4b892d9d3bbf2b1f4ba201c71387cc04d
rainymayday <rainy.mayday2013@gmail.com>
2015-12-09 17:06:30 +0800

    choose by segmenet; compare staff performance in the same segment

--- ui.R
+++ ui.R
@@ -1,28 +1,33 @@
 library(shiny)
 df <- source('Leads.R')
 df <- as.data.frame(df[1])
-names(df) <- c("Date.Created","Lead.Generator", "freq")
-
+names(df) <- c("Date.Created","Lead.Generator","segment", "freq")
 shinyUI(fluidPage(
   
-  titlePanel("Sales Lead report"),
+  titlePanel("Sales Activity report"),
   
   sidebarLayout(
     sidebarPanel(
-    selectInput('y.factor', 
+
+      selectInput('segment', 
+                    'segment', 
+                    unique(as.character(df$segment))),
+
+      selectInput('y.factor', 
                   'sales_rep', 
-                  unique(as.character(df$Lead.Generator))),
-    dateRangeInput('dateRange',
-                   label = 'Date range input: yyyy-mm-dd',
-                   start = Sys.Date() - 14, end = Sys.Date() 
-    )
+                  unique(as.character(df$Lead.Generator)),
+                  selectize = TRUE,multiple = TRUE),
+      dateRangeInput('dateRange',
+                     label = 'Date range input: yyyy-mm-dd',
+                     start = Sys.Date() - 14, end = Sys.Date() 
+      )
+      
 
     ),
-    
-    
+
     mainPanel(
       tabsetPanel(type="tab",
-                  tabPanel("LeadsPlot",plotOutput("LeadsPlot")),
+                  tabPanel("LeadsPlot by day",plotOutput("LeadsPlot")),
                   tabPanel("LeadsPlot by Week",plotOutput("LeadsPlot_by_week")),
                   tabPanel("LeadsTable",dataTableOutput("LeadsTable")))
                   
