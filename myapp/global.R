413b8ae95b1ff887ad72ec78f831a20578e6b9ad
qian <rainy.mayday2013@gmail.com>
2016-03-02 21:32:31 +0800

    update plot

--- myapp/global.R
+++
@@ -1,8 +0,0 @@
-
-factor2numeric <- function(x){
-  x.new <- as.numeric(gsub(",","",as.character(x)))
-  return (x.new)
-}
-merge.all <- function(x, y) {
-  merge(x, y, all=TRUE, by=c("Created.By","Segment"))
-}
